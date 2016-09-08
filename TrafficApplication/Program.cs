ae6584a6871950925286dae31ee66bf2f5f35a40
Nathan Foss <nathandfoss@gmail.com>
2016-09-08 17:03:59 -0400

    Program outputs vehicles as points on a graph GUI

--- TrafficApplication/Program.cs
+++ TrafficApplication/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using System.Windows.Forms;
 
 namespace TrafficApplication
 {
@@ -8,55 +6,9 @@ namespace TrafficApplication
     {
         static void Main(string[] args)
         {
-            Vehicle tempVehicle;
-            int semiCount = 0;
-            int minivanCount = 0;
-            int compactCount = 0;
-            int motorcycleCount = 0;
-            int sedanCount = 0;
-            int pickupCount = 0;
-            TrafficBuilder builder = new TrafficBuilder(2, 65, 50);
-            List<Vehicle> vehicles = builder.GetVehicles();
-            Vehicle[] traffic = vehicles.ToArray();
-            for(int i = 0; i < traffic.Length; i++)
-            {
-                tempVehicle = traffic[i];
-                if(tempVehicle.GetVehicleType() == Type.COMPACT)
-                {
-                    compactCount++;
-                }
-                else if(tempVehicle.GetVehicleType() == Type.SEDAN)
-                {
-                    sedanCount++;
-                }
-                else if (tempVehicle.GetVehicleType() == Type.MINIVAN)
-                {
-                    minivanCount++;
-                }
-                else if (tempVehicle.GetVehicleType() == Type.PICKUP)
-                {
-                    pickupCount++;
-                }
-                else if (tempVehicle.GetVehicleType() == Type.SEMI)
-                {
-                    semiCount++;
-                }
-                else
-                {
-                    motorcycleCount++;
-                }
-
-
-            }
-
-            Console.WriteLine(traffic.Length + " Vehicles");
-            Console.WriteLine(semiCount + " Semis");
-            Console.WriteLine(sedanCount + " Sedans");
-            Console.WriteLine(compactCount + " Compacts");
-            Console.WriteLine(minivanCount + " Vans");
-            Console.WriteLine(pickupCount + " Pickups");
-            Console.WriteLine(motorcycleCount + " Motorcycles");
-            Console.Read();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TrafficForm());
         }
     }
 }
---
+++ TrafficApplication/TrafficForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficApplication
+{
+    public partial class TrafficForm : Form
+    {
+        public TrafficForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TrafficForm_Load(object sender, EventArgs e)
+        {
+            Vehicle tempVehicle;
+            TrafficBuilder builder = new TrafficBuilder(2, 65, 50);
+            List<Vehicle> vehicles = builder.GetVehicles();
+            List<Point> points = new List<Point>();
+            Vehicle[] traffic = vehicles.ToArray();
+
+            for (int i = 0; i < traffic.Length; i++)
+            {
+                tempVehicle = traffic[i];
+                int x = tempVehicle.GetLane();
+                int y = Convert.ToInt32(tempVehicle.GetPosition() * 5280);
+                Point point = new Point(x, y);
+                points.Add(point);
+            }
+
+            Point[] pointsArray = points.ToArray();
+
+            for (int i = 0; i < pointsArray.Length; i++)
+            {
+                CreateGraphics().FillRectangle(new SolidBrush(Color.Black), pointsArray[i].X, pointsArray[i].Y, 1, 1);
+            }
+        }
+    }
+
+}
