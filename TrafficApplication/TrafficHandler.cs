db88133fe0af49ab1d6c5ca44ce32557ddd45933
Nathan Foss <nathandfoss@gmail.com>
2016-09-14 13:50:22 -0400

    Prevent vehicle collisions by looking for vehicles ahead and adjusting velocity

--- TrafficApplication/TrafficHandler.cs
+++ TrafficApplication/TrafficHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TrafficApplication
 {
@@ -18,23 +19,87 @@ namespace TrafficApplication
 
         public List<Vehicle> IterateTraffic()
         {
-            //AdjustVelocity(Vehicles);
+            AdjustVelocity(Vehicles);
             AdvancePosition(Vehicles);
+            SortList(Vehicles);
             return Vehicles;
         }
 
-        public bool IsCarAhead(List<Vehicle> vehicles)
+        private void SortList(List<Vehicle> vehicles)
         {
-            return false; //Compare each car to the car ahead of it
+            Vehicles = vehicles.OrderBy(o => o.GetPosition()).ToList();
+        }
+
+        /// <summary>
+        /// If a vehicle is approaching a vehicle ahead of them, returns the index of the vehicle ahead
+        /// </summary>
+        /// <param name="index">Index in the list of current vehicle</param>
+        /// <param name="vehicles">Sorted list of vehicles on the road</param>
+        /// <returns></returns>
+        private int CarAheadIndex(int index, List<Vehicle> vehicles)
+        {
+            int positionDifference = 0;
+            int drivingLane = vehicles[index].GetLane();
+            int position = vehicles[index].GetPosition();
+            PersonalityHandler handler = new PersonalityHandler(vehicles[index].GetDriver().GetPersonality());
+            int drivingGap = handler.GetDrivingGap();
+
+            for (int i = 0; i < vehicles.Count; i++) 
+            { //look for the next vehicle in the same lane excluding this vehicle
+                //is possible that vehicle initially behind passes current vehicle then
+                //is slowed by traffic ahead
+                
+                if(index != i)
+                {
+                    if(vehicles[i].GetLane() == drivingLane && vehicles[i].GetPosition() > position)
+                    {//get the first vehicle that is in the same lane and ahead of current vehicle
+                        positionDifference = vehicles[i].GetPosition() - position;
+                        if(positionDifference <= drivingGap)
+                        {
+                            return i;
+                        }
+                        break;
+                    }
+                }
+                
+            }
+
+            return -1; //Compare this car to the car ahead of it
+        }
+
+        /// <summary>
+        /// Sets the velocity of an approaching vehicle to the velocity of the vehicle ahead of it
+        /// </summary>
+        /// <param name="carBehind">Index in the list of the current vehicle</param>
+        /// <param name="carAhead">Index in the list of the next vehicle ahead in the same lane</param>
+        /// <param name="vehicles">Sorted list of vehicles on the road</param>
+        private void SlowDown(int carBehind, int carAhead, List<Vehicle> vehicles)
+        {
+            vehicles[carBehind].SetActualVelocity(vehicles[carAhead].GetVelocity());
         }
         
-        //if car ahead either slow down or change lanes
+        /// <summary>
+        /// Uses CarAheadIndex method to check for approaching vehicles. If found, calls SlowDown method
+        /// </summary>
+        /// <param name="vehicles">The sorted list of vehicles on the road</param>
         private void AdjustVelocity(List<Vehicle> vehicles)
         {
-
+            int index;
+            for(int i = 0; i < vehicles.Count; i++)
+            {
+                index = CarAheadIndex(i, vehicles);
+                if (index != -1)
+                    {
+                        SlowDown(i, index, vehicles);
+                    }
+                }
+            
         }
 
-        //
+        /// <summary>
+        /// Increments each vehicle's position based on velocity and 30 frames per second
+        /// </summary>
+        /// <param name="vehicles">The complete collection of vehicles on the road</param>
         private void AdvancePosition(List<Vehicle> vehicles)
         {
             Vehicle tempVehicle;
